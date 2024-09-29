7e7ce94b92a7a8b2e3ad0b6e2c53f528b160a966
karim <karim.tarek96@yahoo.com>
2024-09-29 17:03:19 +0300

    created db connection in the db.js file

---
+++ Backend/config/db.js
@@ -0,0 +1,22 @@
+//* import packages
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+//* create connection to the database
+
+const connectDB = async () => {
+    try {
+        const connection = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected successfully');
+
+    } catch (error) {
+        console.error('MongoDB connection failed:', error.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
