f5500b1ef3eab4c6e79b55a1ed61c54c50369487
karim <karim.tarek96@yahoo.com>
2024-09-29 17:34:39 +0300

    added services class in the userService.js with complete implementation

---
+++ Backend/features/users/services/userService.js
@@ -0,0 +1,53 @@
+//* import packages
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+//* user service class
+class UserService {
+    //* [method] create user
+    //* args: name[String] , password[String]
+    //* return: a promise that resolves to the document saved
+    //* notes: encrypts the user password and store it in the db
+
+    static async createUser(name, password) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ name, password: hashedPassword });
+        return await user.save();
+    }
+
+    //* [method] find user by name
+    //* args: name[String] 
+    //* return: a promise that resolves to the document found 
+    //* notes: uses mongoose built in function to search for a document using the name
+
+    static async findUserByName(name) {
+        return await User.findOne({ name });
+    }
+    //* [method] find user by Id
+    //* args: userId[String] 
+    //* return: a promise that resolves to the document found 
+    //* notes: uses mongoose built in function to search for a document using the userId
+
+    static async findUserById(userId) {
+        return await User.findById(userId);
+    }
+
+    //* [method] update user data
+    //* args: userId[String], updateData [userSchema(name,password,createdAt,updatedAt)] 
+    //* return: a promise that resolves to the document to be updated 
+
+    static async updateUser(userId, updateData) {
+        return await User.findByIdAndUpdate(userId, updateData, { new: true });
+    }
+
+    //* [method] delete user from db
+    //* args: userId[String] 
+    //* return: a promise that resolves to the document to be deleted 
+
+    static async deleteUser(userId) {
+        return await User.findByIdAndDelete(userId);
+    }
+
+}
+
+module.exports = UserService;
